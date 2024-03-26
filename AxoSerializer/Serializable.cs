af110282fd63f73cebcee85832e5d2cb5ed29310
AXOLOTsh <ksda989@gmail.com>
2024-03-26 08:22:35 +0300

    Добавьте файлы проекта.

---
+++ AxoSerializer/Serializable.cs
@@ -0,0 +1,50 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace AxoSerializer
+{
+    /// <summary>
+    /// Represents the base class for classes that need to be serialized.
+    /// </summary>
+    /// <typeparam name="T">Class to serialize (specify the heir).</typeparam>
+    public abstract class Serializable<T> where T : new()
+    {
+        /// <summary>
+        /// Get or set serializer options.
+        /// </summary>
+        [JsonIgnore]
+        public JsonSerializerOptions SerializerOptions { get; set; } = new JsonSerializerOptions()
+        {
+            WriteIndented = true
+        };
+        /// <summary>
+        /// Get or set the path to the directory where the serialization file will be written.
+        /// </summary>
+        public string SerializationPath { get; set; } = AppDomain.CurrentDomain.BaseDirectory;
+
+        /// <summary>
+        /// Serializes a object to a file.
+        /// </summary>
+        /// <param name="serializationObject">The class to be serialized.</param>
+        public virtual void Serialize(T serializationObject)
+        {
+            Serializer.Serialize(serializationObject, Path.Combine(SerializationPath, $"{typeof(T).Name}.json"), SerializerOptions);
+        }
+
+        /// <summary>
+        /// Deserializes a object from a file.
+        /// </summary>
+        /// <returns>Deserialized object.</returns>
+        public T Deserialize()
+        {
+            try
+            {
+                return (T)Serializer.Deserialize(Path.Combine(SerializationPath, $"{typeof(T).Name}.json"));
+            }
+            catch
+            {
+                return new T();
+            } 
+        }
+    }
+}
---
+++ AxoSerializer/Serializer.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.Json;
+
+namespace AxoSerializer
+{
+    /// <summary>
+    /// Static class for serializing objects.
+    /// </summary>
+    public static class Serializer
+    {
+        /// <summary>
+        /// Serializes an object.
+        /// </summary>
+        /// <param name="serializationObject">Serialization object.</param>
+        /// <param name="filePath">The file path where to save the object.</param>
+        /// <param name="serializerOptions">Serializer options.</param>
+        public static void Serialize(object serializationObject, string filePath, JsonSerializerOptions serializerOptions)
+        {
+            File.WriteAllText(filePath, JsonSerializer.Serialize(serializationObject, serializerOptions));
+        }
+
+        /// <summary>
+        /// Deserializes an object.
+        /// </summary>
+        /// <param name="filePath">Path to the file to be deserialized.</param>
+        /// <returns></returns>
+        /// <exception cref="FileNotFoundException">Exception thrown if file not found.</exception>
+        public static object Deserialize(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                return JsonSerializer.Deserialize<object>(File.ReadAllText(filePath));
+            }
+            else
+            {
+                throw new FileNotFoundException($"File {filePath} not found.");
+            }
+        }
+    }
+}
